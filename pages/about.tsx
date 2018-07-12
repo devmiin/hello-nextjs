bf4910474f8e8dfe4da4c0cbab89aa9b42f79db0
Min <min@Mins-Mac-mini.local>
2018-07-12 16:02:48 +0700

    implement redux in about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default class extends React.Component<any, any> {
+class AboutPage extends React.Component<any, any> {
   constructor(props) {
     super(props);
   }
 
   render() {
+    const { userMessage } = this.props;
     return(
       <div>         
+        {userMessage}
          About page
       </div>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  userMessage: state.user.message,
+})
+
+export default connect(mapStateToProps)(AboutPage)
