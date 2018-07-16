37ae0c6a412fa70d226b39b4da55e1d8fcd00727
Min <min@Mins-Mac-mini.BuzzwooRouter>
2018-07-16 17:48:24 +0700

    simulate test

---
+++ __tests__/pages/about.spec.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { mount } from 'enzyme';
+import AboutPage from '../../pages/about'
+
+describe('Pages', () => {
+  describe('AboutPage', () => {
+    it('on button click changes text', () => {
+      const wrapper = mount(<AboutPage />)
+      const button = wrapper.find('button')
+      expect(button.text()).toBe('off')
+      
+      button.simulate('click')
+      expect(button.text()).toBe('on')
+    })
+
+    it('on input changes text', () => {
+      const wrapper = mount(<AboutPage />)
+      const input = wrapper.find('input')
+      expect(wrapper.find('h3').text()).toBe('')
+
+      input.simulate('change', {target: {value: 'Tyler'}})
+      expect(wrapper.find('h3').text()).toBe('Tyler')
+    })
+  })
+})
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,35 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import * as React from 'react'
 
-class AboutPage extends React.Component<any, any> {
+interface IState {
+  on: boolean
+  input: string
+}
+
+export default class extends React.Component<any, IState> {
   constructor(props) {
     super(props);
+    this.state = {
+      on: false,
+      input: ''
+    }
   }
 
   render() {
-    const { userMessage } = this.props;
+    const { on, input } = this.state
     return(
-      <div>         
-        {userMessage}
+      <div>
          About page
+
+         <button onClick={() => this.setState({ on: !on })}>
+          {on? 'on': 'off'}
+         </button>
+
+          <h3>{input}</h3>
+          <input 
+            onChange={(e) => this.setState({input: e.target.value})} 
+            type='text' 
+          />
       </div>
     )
   }
 }
-
-const mapStateToProps = state => ({
-  userMessage: state.user.message,
-})
-
-export default connect(mapStateToProps)(AboutPage)
